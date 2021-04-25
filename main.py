bdf91c4426d915692385b373a1ec2d2bb1b1567b
Dennis Lypovetsky <tsssssssss@gmail.com>
2021-04-25 15:54:23 +0300

    to list

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import yfinance as yf
+import pandas as pd
 
-myTicker = yf.Ticker("BTC-USD")
+btc = yf.Ticker("BTC-USD")
 
-# get historical market data
-hist = myTicker.history(period="5d")
-print(hist)
+btcHist = btc.history(start="2020-03-01")
+btcHistToList = pd.DataFrame(btcHist).values.tolist()
+
+print(btcHist)
+print("--------------------")
+print(btcHistToList[1][0])
+print("--------------------")
+print(btcHistToList[-1][0])
+
+# print(btcHist)
+# print(btcHist[1])
+# # print(btcHist[1, 1])
+
+
+
